b2c1adca223439698470e1a497494f8d524e7669
Joshua Adams <n7down@gmail.com>
2016-10-21 16:50:37 -0600

    added socket.io

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Tanks
-Tanks is a HTML5 multiplayer game.
+Tanks is a HTML5 multiplayer game. This is a work in progress.
+
+## Prerequisites
+1. [Nodejs](https://nodejs.org/en/)
+2. [Npm](https://www.npmjs.com)
+
+## Running the server
+1. Run `npm install`
+2. Run `npm start`
+3. Connect to the game on `localhost:2000`
+
+## Todo
+Finish this [video](https://www.youtube.com/watch?annotation_id=annotation_4117949269&feature=iv&src_vid=_GioD4LpjMw&v=hO14DHHpgxw)
---
+++ app.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/client/index.html');
+});
+app.use('/client', express.static(__dirname + '/client'));
+
+server.listen(2000);
+console.log("server started");
+
+var SOCKET_LIST = {};
+
+var io = require('socket.io')(server, {});
+io.sockets.on('connection', function(socket) {
+  socket.id = Math.random();
+  socket.x = 0;
+  socket.y = 0;
+  socket.number = "" + Math.floor(10 * Math.random());
+  SOCKET_LIST[socket.id] = socket;
+  console.log('socket connection');
+
+  socket.on('disconnect', function() {
+    delete SOCKET_LIST[socket.id];
+  })
+});
+
+setInterval(function() {
+  var pack = [];
+  for(var i in SOCKET_LIST) {
+    var socket = SOCKET_LIST[i];
+    socket.x++;
+    socket.y++;
+    pack.push({
+      x: socket.x,
+      y: socket.y,
+      number: socket.number
+    });
+  }
+  for(var i in SOCKET_LIST) {
+    var socket = SOCKET_LIST[i];
+    socket.emit('newPositions', pack);
+  }
+}, 1000/25);
