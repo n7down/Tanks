628e03d65844f790cac55e0e620b5eeaef2335e6
Joshua Adams <n7down@gmail.com>
2016-10-24 02:08:51 -0600

    implemented keypress

--- app.js
+++ app.js
@@ -11,31 +11,76 @@ server.listen(2000);
 console.log("server started");
 
 var SOCKET_LIST = {};
+var PLAYER_LIST = {};
+
+var Player = function(id) {
+  var self = {
+    x: 250,
+    y: 250,
+    id: id,
+    number: "" + Math.floor(10 * Math.random()),
+    pressingRight: false,
+    pressingLeft: false,
+    pressingUp: false,
+    pressingDown: false,
+    maxSpeed: 10
+  }
+  self.updatePosition = function() {
+    if(self.pressingRight) {
+      self.x += self.maxSpeed;
+    }
+    if(self.pressingLeft) {
+      self.x -= self.maxSpeed;
+    }
+    if(self.pressingUp) {
+      self.y -= self.maxSpeed;
+    }
+    if(self.pressingDown) {
+      self.y += self.maxSpeed;
+    }
+  }
+  return self;
+}
 
 var io = require('socket.io')(server, {});
 io.sockets.on('connection', function(socket) {
   socket.id = Math.random();
-  socket.x = 0;
-  socket.y = 0;
-  socket.number = "" + Math.floor(10 * Math.random());
   SOCKET_LIST[socket.id] = socket;
   console.log('socket connection');
 
+  var player = Player(socket.id);
+  PLAYER_LIST[socket.id] = player;
+
   socket.on('disconnect', function() {
     delete SOCKET_LIST[socket.id];
+    delete PLAYER_LIST[socket.id];
+  })
+
+  socket.on('keyPress', function(data) {
+    if(data.inputId == 'left') {
+      player.pressingLeft = data.state;
+    }
+    else if(data.inputId == 'right') {
+      player.pressingRight = data.state;
+    }
+    else if(data.inputId == 'up') {
+      player.pressingUp = data.state;
+    }
+    else if(data.inputId == 'down') {
+      player.pressingDown = data.state;
+    }
   })
 });
 
 setInterval(function() {
   var pack = [];
-  for(var i in SOCKET_LIST) {
-    var socket = SOCKET_LIST[i];
-    socket.x++;
-    socket.y++;
+  for(var i in PLAYER_LIST) {
+    var player = PLAYER_LIST[i];
+    player.updatePosition();
     pack.push({
-      x: socket.x,
-      y: socket.y,
-      number: socket.number
+      x: player.x,
+      y: player.y,
+      number: player.number
     });
   }
   for(var i in SOCKET_LIST) {
